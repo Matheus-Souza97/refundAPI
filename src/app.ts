20f73dcc21de9c4616f53b01ff681ee22cfe3b7e
Matheus-Souza97 <matheus-souza1@outlook.com.br>
2026-07-07 16:59:04 -0300

    feat: configure Prisma and create initial database schema

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from "express"
+import cors from "cors"
+import { errorHandling } from "./middlewares/error-handling"
+import { AppError } from "./utils/AppError"
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
-app.get("/", (req, res) => {
-  res.send("Hello World")
-})
+app.use(errorHandling)
   
 
 export { app }
